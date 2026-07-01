6217064a1532fb2c1fd67bd6b49bdcccf300c1d4
Elias <garciaelias382@gmail.com>
2026-06-30 21:28:08 -0300

    Limpieza de archivos estáticos antiguos para iniciar refactorización a PHP

--- links/scriptContacto.js
+++
@@ -1,8 +0,0 @@
-//Redirige al usuario a la página de inicio cuando hace click en el logo de la página.
-
-let imagen =document.getElementById("logoBoton");
-
-
-imagen.addEventListener("click", function() {
-    location.href = "../index.html";
-});
--- links/scriptInfo.js
+++
@@ -1,6 +0,0 @@
-
-let imagen = document.getElementById("logoBoton");
-
-imagen.addEventListener("click", () => {
-    location.href = "../index.html";
-});
